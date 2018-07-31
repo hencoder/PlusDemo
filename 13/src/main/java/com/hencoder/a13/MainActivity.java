33b43c24b54641528a5ba9a01bbdd5f82df479a9
Kai Zhu <rengwuxian@gmail.com>
2018-07-31 22:37:54 +0800

    13

---
+++ 13/src/main/java/com/hencoder/a13/MainActivity.java
@@ -0,0 +1,13 @@
+package com.hencoder.a13;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.drag_helper_grid_view);
+    }
+}
---
+++ 13/src/main/java/com/hencoder/a13/view/DragToCollectLayout.java
@@ -0,0 +1,65 @@
+package com.hencoder.a13.view;
+
+import android.content.ClipData;
+import android.content.Context;
+import android.support.v4.view.ViewCompat;
+import android.util.AttributeSet;
+import android.view.DragEvent;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.hencoder.a13.R;
+
+public class DragToCollectLayout extends RelativeLayout {
+    ImageView avatarView;
+    ImageView logoView;
+    LinearLayout collectorLayout;
+
+    OnLongClickListener dragStarter = new OnLongClickListener() {
+        @Override
+        public boolean onLongClick(View v) {
+            ClipData imageData = ClipData.newPlainText("name", v.getContentDescription());
+            return ViewCompat.startDragAndDrop(v, imageData, new DragShadowBuilder(v), null, 0);
+        }
+    };
+    OnDragListener dragListener = new CollectListener();
+
+    public DragToCollectLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    protected void onFinishInflate() {
+        super.onFinishInflate();
+
+        avatarView = findViewById(R.id.avatarView);
+        logoView = findViewById(R.id.logoView);
+        collectorLayout = findViewById(R.id.collectorLayout);
+
+        avatarView.setOnLongClickListener(dragStarter);
+        logoView.setOnLongClickListener(dragStarter);
+        collectorLayout.setOnDragListener(dragListener);
+    }
+
+    class CollectListener implements OnDragListener {
+        @Override
+        public boolean onDrag(View v, DragEvent event) {
+            switch (event.getAction()) {
+                case DragEvent.ACTION_DROP:
+                    if (v instanceof LinearLayout) {
+                        LinearLayout layout = (LinearLayout) v;
+                        TextView textView = new TextView(getContext());
+                        textView.setTextSize(16);
+                        textView.setText(event.getClipData().getItemAt(0).getText());
+                        layout.addView(textView);
+                    }
+                    break;
+            }
+            return true;
+        }
+    }
+
+}
